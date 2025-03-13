af2fa5f3f7d0d97a9faf56ad6e99122653f42924
Nozeren <45057714+Nozeren@users.noreply.github.com>
2025-03-13 19:03:48 +0200

    Add remove row and status code conditon

--- app/router/transactions.py
+++ app/router/transactions.py
@@ -1,17 +1,32 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, status, HTTPException
 from db.queries.transactions import Transactions, TransactionsModel
 import json
 router = APIRouter(prefix="/transactions")
+
 
 @router.get('/')
 async def get_all_transactions():
     return await Transactions().get_all_transactions()
 
+
 @router.put('/')
-async def update_row_value(response:dict):
+async def update_row_value(response: dict):
     return await Transactions().update_value(row_id=int(response['row_id']), column=response['column'], value=response['value'])
 
-@router.post('/')
-async def add_row(transaction:TransactionsModel):
-    return await Transactions().add_row(transaction)
 
+@router.post('/', status_code=status.HTTP_201_CREATED)
+async def add_row(transaction: TransactionsModel) -> TransactionsModel:
+    row = await Transactions().add_row(transaction)
+    if not row:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"{id} content not found!")
+    return row
+
+
+@router.delete('/{id}', status_code=status.HTTP_200_OK)
+async def delete_row(id: int):
+    row = await Transactions().delete_row(id)
+    if not row:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"{id} content not found!")
+    return row
--- db/queries/transactions.py
+++ db/queries/transactions.py
@@ -1,23 +1,25 @@
 from pydantic import BaseModel, StrictStr
-from db.database import database 
+from db.database import database
 from typing import Optional, Union
 from datetime import datetime
 
 
 class TransactionsModel(BaseModel):
     id: Optional[int] = None
-    posted_date: Union[StrictStr, datetime] 
-    description: str 
-    user_id:int 
+    posted_date: Union[StrictStr, datetime]
+    description: str
+    user_id: int
     bank_id: int
-    subcategory_id: int 
-    shared_amount: float 
-    amount:float
+    subcategory_id: int
+    shared_amount: float
+    amount: float
+
 
 class Transactions():
     def __init__(self):
         self.database = database
-    async def get_subcategory_id_by_name(self, subcategory_name:str): 
+
+    async def get_subcategory_id_by_name(self, subcategory_name: str):
         query = f"""SELECT id FROM subcategories WHERE position('{subcategory_name}' in name)>0"""
         async with self.database.pool.acquire() as conn:
             return await conn.fetchrow(query)
@@ -33,23 +35,35 @@ class Transactions():
         async with self.database.pool.acquire() as conn:
             rows = await conn.fetch(query)
             rows = [row for row in rows]
-            return rows 
+            return rows
 
-    async def update_value(self, row_id:int, column, value):
-        if isinstance(value,str):
+    async def update_value(self, row_id: int, column, value):
+        if isinstance(value, str):
             query = f"UPDATE transactions SET {column} = '{value}' WHERE id = {row_id} RETURNING *"
         elif isinstance(value, int):
             query = f"UPDATE transactions SET {column} = {value} WHERE id = {row_id} RETURNING *"
         async with self.database.pool.acquire() as conn:
             return await conn.execute(query)
-    
-    async def add_row(self, transaction:TransactionsModel):
+
+    async def add_row(self, transaction: TransactionsModel):
         query = f"""INSERT INTO transactions(posted_date, description, user_id, bank_id, subcategory_id, shared_amount, amount)
                 VALUES('{transaction.posted_date}',$1,$2,$3,$4,$5,$6) RETURNING *;"""
         async with self.database.pool.acquire() as conn:
-            result = await conn.fetchrow(query, transaction.description, 
-                                      transaction.user_id, transaction.bank_id, transaction.subcategory_id,
-                                      transaction.shared_amount, transaction.amount)
-            return TransactionsModel(id=result['id'],posted_date=datetime.strftime(result['posted_date'], '%Y-%m-%d'), description=result['description'], user_id=result['user_id'], bank_id=result['bank_id'], 
-                                     subcategory_id=result['subcategory_id'], shared_amount=result['shared_amount'], 
-                                     amount=result['amount'])
+            result = await conn.fetchrow(query, transaction.description,
+                                         transaction.user_id, transaction.bank_id, transaction.subcategory_id,
+                                         transaction.shared_amount, transaction.amount)
+            if result is not None:
+                return TransactionsModel(id=result['id'], posted_date=datetime.strftime(result['posted_date'], '%Y-%m-%d'), description=result['description'], user_id=result['user_id'], bank_id=result['bank_id'],
+                                         subcategory_id=result['subcategory_id'], shared_amount=result['shared_amount'],
+                                         amount=result['amount'])
+            return None
+
+    async def delete_row(self, id: int):
+        query = "DELETE FROM transactions WHERE id = $1 RETURNING *;"
+        async with database.pool.acquire() as conn:
+            result = await conn.fetchrow(query, id)
+            if result is not None:
+                return TransactionsModel(id=result['id'], posted_date=datetime.strftime(result['posted_date'], '%Y-%m-%d'), description=result['description'], user_id=result['user_id'], bank_id=result['bank_id'],
+                                         subcategory_id=result['subcategory_id'], shared_amount=result['shared_amount'],
+                                         amount=result['amount'])
+            return None
