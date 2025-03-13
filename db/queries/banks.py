afceccce8124b265acf9dd83baa73bf05e02384b
Nozeren <45057714+Nozeren@users.noreply.github.com>
2025-03-13 20:30:30 +0200

    Add color to banks

--- db/queries/banks.py
+++ db/queries/banks.py
@@ -1,5 +1,5 @@
 from pydantic import BaseModel
-from db.database import database 
+from db.database import database
 from typing import Optional
 import logging
 
@@ -8,24 +8,27 @@ class BankModel(BaseModel):
     id: Optional[int] = None
     name: str
     country: str
+    color: str
+
 
 class Banks():
     def __init__(self):
         self.database = database
-    
+
     async def get_all_banks(self) -> list[BankModel]:
         query = "SELECT * FROM banks"
         async with self.database.pool.acquire() as conn:
             rows = await conn.fetch(query)
-            data = [BankModel(id=row['id'], name=row['name'], country=row['country']) for row in rows]
-            return data 
+            data = [BankModel(id=row['id'], name=row['name'],
+                              country=row['country'], color=row['color']) for row in rows]
+            return data
 
-    async def insert_bank(self, bank:BankModel):
-        query = "INSERT INTO banks (name, country) VALUES ($1, $2) RETURNING *"
+    async def insert_bank(self, bank: BankModel):
+        query = "INSERT INTO banks (name, country, color) VALUES ($1, $2, $3) RETURNING *"
         async with database.pool.acquire() as conn:
-            await conn.execute(query, bank.name, bank.country )
+            await conn.execute(query, bank.name, bank.country, bank.color)
 
-    async def get_bank_by_id(self, id:int):
+    async def get_bank_by_id(self, id: int):
         query = "SELECT * FROM banks WHERE id = $1"
         async with self.database.pool.acquire() as conn:
             return await conn.fetchrow(query, id)
