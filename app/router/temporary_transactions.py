a8fd113aa460e537ab8594e1111d747c9bb30916
Nozeren <45057714+Nozeren@users.noreply.github.com>
2025-03-24 16:43:02 +0200

    add router to temporary_transactions

--- app/router/temporary_transactions.py
+++ app/router/temporary_transactions.py
@@ -1,13 +1,13 @@
 import json
 from datetime import datetime
-from fastapi import APIRouter
-from db.queries.temporary_transactions import TemporaryTransactions 
+from fastapi import APIRouter, HTTPException, status
+from db.queries.temporary_transactions import TemporaryTransactions
 import logging
 router = APIRouter(prefix="/temporary-transactions")
 
 
 @router.post('/')
-async def upload_transactions(response:dict):
+async def upload_transactions(response: dict):
     # The goal here is to add dataset to DB
     data = list(json.loads(response['data']))
     columns = list(json.loads(response['columns']))
@@ -22,7 +22,26 @@ async def upload_transactions(response:dict):
             elif column == "amount":
                 row[index] = float(row[index])
 
-    temporary_transactions = TemporaryTransactions() 
-    await temporary_transactions.insert_data(data=data,columns=columns)
+    temporary_transactions = TemporaryTransactions()
+    await temporary_transactions.insert_data(data=data, columns=columns)
     await temporary_transactions.remove_duplicate()
     await temporary_transactions.add_subcategory()
+
+
+@router.get('/')
+async def get_all_transactions():
+    return await TemporaryTransactions().get_all_transactions()
+
+
+@router.put('/')
+async def update_row_value(response: dict):
+    return await TemporaryTransactions().update_value(row_id=int(response['row_id']), column=response['column'], value=response['value'])
+
+
+@router.delete('/{id}', status_code=status.HTTP_200_OK)
+async def delete_row(id: int):
+    row = await TemporaryTransactions().delete_row(id)
+    if not row:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"{id} content not found!")
+    return row
--- db/queries/temporary_transactions.py
+++ db/queries/temporary_transactions.py
@@ -1,17 +1,28 @@
-from pydantic import BaseModel
-from db.database import database 
-from typing import Optional
-import pandas as pd
+from pydantic import BaseModel, StrictStr
+from db.database import database
+from typing import Optional, Union
+from datetime import datetime
+
+
+class TemporaryTransactionsModel(BaseModel):
+    id: Optional[int] = None
+    posted_date: Union[StrictStr, datetime]
+    description: str
+    user_id: int
+    bank_id: int
+    subcategory_id: int
+    shared_amount: float
+    amount: float
 
 
 class TemporaryTransactions():
     def __init__(self):
         self.database = database
+
     async def insert_data(self, data: list, columns: list):
         async with database.pool.acquire() as conn:
             await conn.copy_records_to_table("temporary_transactions", records=data, columns=columns)
 
-    
     async def remove_duplicate(self):
         query = """DELETE FROM temporary_transactions t
                     USING transactions tr
@@ -23,8 +34,7 @@ class TemporaryTransactions():
         async with database.pool.acquire() as conn:
             await conn.execute(query)
 
-    
-    async def add_subcategory(self): 
+    async def add_subcategory(self):
         # Add subcategory if description already in acitivity table
         query = f"""UPDATE temporary_transactions
                     SET subcategory_id = transactions.subcategory_id
@@ -32,3 +42,34 @@ class TemporaryTransactions():
                     WHERE temporary_transactions.description = transactions.description;"""
         async with database.pool.acquire() as conn:
             await conn.execute(query)
+
+    async def get_all_transactions(self):
+        query = f"""SELECT t.id, t.posted_date, t.description, t.user_id, u.name as user_name, subcategory_id, s.name as subcategory_name, shared_amount , bank_id, b.name as bank_name, amount from temporary_transactions t
+                        left join subcategories as s
+                        on t.subcategory_id = s.id
+                        left join banks as b
+                        on t.bank_id = b.id
+                        left join users as u
+                        on t.user_id= u.id"""
+        async with self.database.pool.acquire() as conn:
+            rows = await conn.fetch(query)
+            rows = [row for row in rows]
+            return rows
+
+    async def update_value(self, row_id: int, column, value):
+        if isinstance(value, str):
+            query = f"UPDATE temporary_transactions SET {column} = '{value}' WHERE id = {row_id} RETURNING *"
+        elif isinstance(value, int):
+            query = f"UPDATE temporary_transactions SET {column} = {value} WHERE id = {row_id} RETURNING *"
+        async with self.database.pool.acquire() as conn:
+            return await conn.execute(query)
+
+    async def delete_row(self, id: int):
+        query = "DELETE FROM temporary_transactions WHERE id = $1 RETURNING *;"
+        async with database.pool.acquire() as conn:
+            result = await conn.fetchrow(query, id)
+            if result is not None:
+                return TemporaryTransactionsModel(id=result['id'], posted_date=datetime.strftime(result['posted_date'], '%Y-%m-%d'), description=result['description'], user_id=result['user_id'], bank_id=result['bank_id'],
+                                                  subcategory_id=result['subcategory_id'], shared_amount=result['shared_amount'],
+                                                  amount=result['amount'])
+            return None
